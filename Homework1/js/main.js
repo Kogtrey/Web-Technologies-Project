84bfa0135e31b53c8723005bb0bbdf8cf5fdeb60
Kody <kodyneill@gmail.com>
2020-01-24 13:26:16 -0700

    Update index.html

--- Homework1/js/main.js
+++ Homework1/js/main.js
@@ -1,1 +1,23 @@
-console.log("At last! I, the console, Lives!")
+
+function indexLog() {
+  console.log("Favorite Games: \n"
+            + "1. Rocket League \n"
+            + "2. Ark: Survival Evloved \n"
+            + "3. Minecraft \n")
+
+  console.log("\nFavorite Websites:: \n"
+            + "1. Reddit.com \n"
+            + "2. Youtube.com \n"
+            + "3. Roosterteeth.com \n")
+
+  console.log("\nFavorite Artists: \n"
+            + "1. twentyonepilots \n"
+            + "1. Green Day \n"
+            + "1. Imagine Dragons \n")
+}
+function page1Log() {
+  console.log("You just enetered page 1");
+}
+function page2Log() {
+  console.log("You just enetered page 2");
+}
