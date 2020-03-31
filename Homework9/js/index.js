18ad82723e5726aa19392edf2d8e3db17ceabb51
Kody <Kodyneill@gmail.com>
2020-03-31 13:23:44 -0600

    17

--- Homework9/js/index.js
+++ Homework9/js/index.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
   $("button").click(function() {
     $(document).load("./data/spacepeople.json", function(responseTxt,statusTxt,xhr){
       json = JSON.parse(responseTxt);
-      for (var i = 0; i < json.people; i++) {
-        people.push(json.people[i].name)
-      }
+      people.push(json.people[0].name)
+      people.push(json.people[1].name)
+      people.pusj(json.people[2].name)
       console.log(people);
       $("#spacePeople").html()
     });
