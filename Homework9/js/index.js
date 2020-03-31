f3d12810c36787d3d933c33d6388f45f99c56f5f
Kody <Kodyneill@gmail.com>
2020-03-31 14:30:59 -0600

    22

--- Homework9/js/index.js
+++ Homework9/js/index.js
@@ -13,8 +13,11 @@ $(document).ready(function() {
       for(var i=0; i < people.length; i++){
         var h2 = document.createElement("h2");
         h2.innerHTML = people[i];
+        h2.classList.add("person")
         div.appendChild(h2)
       }
+
+      div.innerHTML = "";
     });
   });
 });
